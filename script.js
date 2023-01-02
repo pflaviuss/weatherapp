be8dfa1f267b39b28f6bdecbf911137973456e2d
Flavius <flavius.popa123@gmail.com>
2023-01-02 23:34:52 +0200

    Figured out how to set the icons, but now I have to figure out how to sync the search with the h1 chosenlocation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const apikey = `45d2b1974108dfa1128c4ce9991a1f56`;
-const city = 'Bucharest';
+let city = 'Constanta';
+// let search = document.querySelector('.searchbar') must figure out how the search is gonna work 
 
 
 
@@ -9,7 +10,7 @@ window.addEventListener('load', ()=>{
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.degree');
     let locationTimezone = document.querySelector('.location-timezone');
-    // let icon = document.querySelector('.icon');
+    let icon = document.querySelector('.icon');
     // const tempdata = await apirequests.relevantData;
     
     if(navigator.geolocation){
@@ -29,18 +30,31 @@ window.addEventListener('load', ()=>{
                 const {temp} = data.main;
                 const {main} = data.weather[0];
                 const {country} = data.sys;
-                
+                // search.e('input') HAVE TO FIGURE OUT HOW THE SEARCH IS GONNA WORK 
+                // city.textContent = search.textContent;
 
                 // set DOM elements from the API
 
                 temperatureDegree.textContent = temp;
                 temperatureDescription.textContent = main;
                 locationTimezone.textContent = country;
-                // figure out cons
 
-                // icon.src = `http://openweathermap.org/img/wn/${tempdata.weather[0].icon}@2x.png`;
-                // icon.setAttribute(`title`, tempdata.weather[0].description);
+                if(temperatureDescription.textContent === 'Mist'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/50d@2x.png';
+                }if(temperatureDescription.textContent === 'Clear Sky'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/01d@2x.png';
+                }if(temperatureDescription.textContent === 'Clouds'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/03d@2x.png';
+                }if(temperatureDescription.textContent === 'Rain'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/10d@2x.png';
+                }if(temperatureDescription.textContent === 'Thunderstorm'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/11d@2x.png';
+                }if(temperatureDescription.textContent === 'Snow'){
+                    document.querySelector('.icon').src='http://openweathermap.org/img/wn/13d@2x.png';
+                }
                
+               console.log(temperatureDescription.textContent);
+        
             });});
             
             
